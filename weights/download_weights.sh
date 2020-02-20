d37fd3e098b618a778d5aaf280426be1b80a3a24
05Bang <45676316+05Bang@users.noreply.github.com>
2020-02-20 12:40:34 +0900

    Add files via upload

---
+++ weights/download_weights.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget -O weights.zip --no-check-certificate https://skku0-my.sharepoint.com/:u:/g/personal/byo7000_skku_edu/ESF_WuwUau5Mrl3hdteSChcBX3mBt8SP1Ajq-CmkEN7MlQ?download=1
+
+unzip weights.zip -d pre-trained_weights
