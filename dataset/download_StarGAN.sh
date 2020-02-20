a95cd8b66fd3532111cd7735ef5b9a747f4512c8
05Bang <45676316+05Bang@users.noreply.github.com>
2020-02-20 12:42:50 +0900

    Delete download_StarGAN.sh

--- dataset/download_StarGAN.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-wget -O StarGAN_128.tar --no-check-certificate https://skku0-my.sharepoint.com/:u:/g/personal/byo7000_skku_edu/Ec3bm71HBKlOgWVFTELisB4BasdhDEP4b3bvW10cvcHZMg?download=1
-
-tar -C ./StarGAN_128
-tar -xvf StarGAN_128.tar
