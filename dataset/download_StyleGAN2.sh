1ac890f99a21ba0eb16fabf30c2efe9d328b14a6
05Bang <45676316+05Bang@users.noreply.github.com>
2020-02-20 12:43:03 +0900

    Delete download_StyleGAN2.sh

--- dataset/download_StyleGAN2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-wget -O StyleGAN2_256.tar.001 --no-check-certificate https://skku0-my.sharepoint.com/:u:/g/personal/byo7000_skku_edu/EQCPywddQ8VLvB5jU_l8NIABCv2Dwv77-5AYLmEa9oRyig?download=1
-
-wget -O StyleGAN2_256.tar.002 --no-check-certificate https://skku0-my.sharepoint.com/:u:/g/personal/byo7000_skku_edu/Ee5Dco4narZFigMyCFnqrc0BnGmNr2E8OH81RbLrlsiQIw?download=1
-
-tar -C ./StyleGAN2_256
-cat StyleGAN2_256.tar* | tar xvf -
