6ed7bb4172d7e98d693176951ccf6dbe7af4ced4
05Bang <45676316+05Bang@users.noreply.github.com>
2020-02-28 16:17:17 +0900

    Add files via upload

---
+++ dataset/download_PGGAN.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget -O PGGAN_128.tar --no-check-certificate https://skku0-my.sharepoint.com/:u:/g/personal/byo7000_skku_edu/EX_S6R5M2w1IrHtr3QLNYD8BzLbXgBNMf38Byx6mnHDXyg?download=1
+
+tar -C ./PGGAN_128 tar -xvf PGGAN_128.tar
+
