34e178e7509c237a36a00d77326e41cf33a2870a
cutz-j <cutz-j@naver.com>
2020-08-07 22:38:46 +0900

    Update pretrain.py

--- pretrain.py
+++ pretrain.py
@@ -61,7 +61,7 @@ val_aug = transforms.Compose([
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
 val_loader = DataLoader(datasets.ImageFolder(val_dir, val_aug),
-                       batch_size=opt.test_batch, shuffle=False, num_workers=opt.num_workers, pin_memory=True)
+                       batch_size=opt.test_batch, shuffle=True, num_workers=opt.num_workers, pin_memory=True)
 
 criterion = nn.CrossEntropyLoss().cuda()
 optimizer = optim.SGD(model.parameters(), lr=opt.lr, momentum=opt.momentum, weight_decay=1e-4)
@@ -120,10 +120,10 @@ def train(opt, train_loader, model, criterion, optimizer, epoch, use_cuda):
 
         # measure accuracy and record loss
         prec1 = accuracy(outputs.data, targets.data)
-#        auroc = roc_auc_score(targets.cpu().detach().numpy(), outputs.cpu().detach().numpy()[:,1])
+        auroc = roc_auc_score(targets.cpu().detach().numpy(), outputs.cpu().detach().numpy()[:,1])
         losses.update(loss.data.tolist(), inputs.size(0))
         top1.update(prec1[0], inputs.size(0))
-#        arc.update(auroc, inputs.size(0))
+        arc.update(auroc, inputs.size(0))
 
         # compute gradient and do SGD step
         optimizer.zero_grad()
@@ -135,10 +135,10 @@ def train(opt, train_loader, model, criterion, optimizer, epoch, use_cuda):
         end = time.time()
         
         if batch_idx % 100 == 0:
-            print('{batch}/{size} | Loss:{loss:.4f} | top1:{tp1:.4f}'.format(
-                     batch=batch_idx+1, size=len(train_loader), loss=losses.avg, tp1=top1.avg))
-    print('{batch}/{size} | Loss:{loss:.4f} | top1:{tp1:.4f}'.format(
-                     batch=batch_idx+1, size=len(train_loader), loss=losses.avg, tp1=top1.avg))
+            print('{batch}/{size} | Loss:{loss:.4f} | top1:{tp1:.4f | AUROC:{ac:.4f}}'.format(
+                     batch=batch_idx+1, size=len(train_loader), loss=losses.avg, tp1=top1.avg, ac=arc.avg))
+    print('{batch}/{size} | Loss:{loss:.4f} | top1:{tp1:.4f} | AUROC:{ac.4f}'.format(
+                     batch=batch_idx+1, size=len(train_loader), loss=losses.avg, tp1=top1.avg, ac=arc.avg))
     return (losses.avg, top1.avg, arc.avg)
 
 def test(opt, val_loader, model, criterion, epoch, use_cuda):
