20dea844c8a608431d2f8e72c58d0d2db300f57a
05Bang <45676316+05Bang@users.noreply.github.com>
2021-05-10 15:48:40 +0900

    Update download_StyleGAN2.sh

--- dataset/download_StyleGAN2.sh
+++ dataset/download_StyleGAN2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget -O StyleGAN2_256.tar --no-check-certificate https://skku0-my.sharepoint.com/:u:/g/personal/byo7000_skku_edu/EV3nfdsCWSZGuE5uE0rFAuoBAqVms-Tn3s34rDn1UJFgjw?download=1
+wget -O StyleGAN2_256.tar --no-check-certificate https://skku0-my.sharepoint.com/:u:/g/personal/byo7000_skku_edu/EZSOgqfry7lJiwcKwBmHH2MBPzqG7wNm-i7Lx5Wk94a3Ug?download=1
 
 tar -C ./StyleGAN2_256
 tar -xvf StyleGAN2_256.tar
